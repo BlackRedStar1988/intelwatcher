f0338b42817e50609b81c3f041d7da52ec125704
Benjamin Schultz <bschultz@proqrent.de>
2020-07-05 22:49:42 +0200

    Try fixing "DevToolsActivePort file doesn't exist" error on Ubuntu

--- selenium_get_ingress_cookie.py
+++ selenium_get_ingress_cookie.py
@@ -1,5 +1,6 @@
 import time
 import argparse
+import random
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,6 +8,22 @@ from util.config import SeleniumConfig
 
 
 def get_ingress_cookie(config):
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3072.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.32',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 BIDUBrowser/8.3 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.9.2.1000 Chrome/39.0.2146.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 61; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.277.400 QQBrowser/9.4.7658.400',
+        'Mozilla/5.0 (Linux; Android 5.0; SM-N9100 Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36 MicroMessenger/6.0.2.56_r958800.520 NetType/WIFI',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 QQ/5.2.1.302 NetType/WIFI Mem/28'
+    ]
+
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
     if config.webdriver == 'firefox':
@@ -16,6 +33,9 @@ def get_ingress_cookie(config):
         options.add_argument(f'user-agent={user_agent}')
 
     options.add_argument("--headless")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
 
     if config.webdriver == 'chromium':
         from webdriver_manager.chrome import ChromeDriverManager
@@ -26,6 +46,13 @@ def get_ingress_cookie(config):
         from webdriver_manager.firefox import GeckoDriverManager
 
         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+    #elif config.webdriver == 'phantomjs':
+    #    dcap = dict(webdriver.DesiredCapabilities.PHANTOMJS)
+    #    dcap['phantomjs.page.settings.userAgent'] = (
+    #        random.choice(user_agents)
+    #    )
+    #    driver = webdriver.PhantomJS(desired_capabilities=dcap)
+    #    driver.set_window_size(1400, 1000)
     else:
         from webdriver_manager.chrome import ChromeDriverManager
 
@@ -44,7 +71,7 @@ def get_ingress_cookie(config):
         driver.implicitly_wait(10)
 
         print('Enter password...')
-        driver.find_element(By.ID, 'password').find_element(By.TAG_NAME, 'input').send_keys(config.ingress_password)
+        driver.find_element(By.ID, 'password').find_element(By.NAME, 'password').send_keys(config.ingress_password)
         driver.find_element(By.ID, 'passwordNext').click()
         driver.implicitly_wait(10)
 
